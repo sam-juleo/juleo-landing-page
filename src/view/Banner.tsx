930604a457c0dbbb2faa00f1ef2708f8d6910771
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-07-04 20:25:25 +0530

    Update Banner.tsx

--- src/view/Banner.tsx
+++ src/view/Banner.tsx
@@ -1,4 +1,5 @@
-
+import { FaApple } from "react-icons/fa6";
+import { FaGooglePlay } from "react-icons/fa";
 function Banner() {
 
     return (
@@ -25,18 +26,27 @@ function Banner() {
                         <div className=" rounded-full m">
 
                         </div>
-                        <h1 className="text-4xl lg:text-7xl font-normal text-center leading-none  px-3 w-3/4 tracking-tighter font-Frank
+                        <h1 className="text-4xl lg:text-7xl -mt-50 font-normal text-center leading-none  px-3 w-3/4 tracking-tighter font-Frank
                         text-white ">
                             It's not a <span className="text-gray-300">dating app</span><br />
                             It's not a <span className="text-gray-300">matrimony app</span>
                             
                             </h1>
                         
-                        <p className="w-3/5 text-center text-xl lg:text-4xl mt-6 tracking-normal text-white font-light ">
+                        <p className="w-11/12 text-center text-xl lg:text-4xl mt-6 tracking-normal text-white font-light ">
                          Juleo is a Trusted Singles Club of Govt ID verified<br/> Members who actually meet, not just match
                         </p>
+                        <div className="flex flex-col h-104  lg:flex-row gap-10 h-36">
                         <button
-                            className="px-4 py-2 lg:px-6 py-3 bg-white text-wine-100 mt-20 rounded-full font-bold lg:font-medium text-xs lg:text-base tracking-tight">Join now</button>
+                            className="px-10 py-3 text-4xl lg:px-10 -py-10 bg-white text-wine-100 mt-20 rounded-full font-semibold lg:font-bold text-xl lg:text-base tracking-tight">Join now
+                        </button>
+                        
+                        <div className=" w-3/2 h-10 mt-24 flex justify-center gap-3 ">
+                            <FaApple className="text-white text-3xl opacity-80" />
+                            <FaGooglePlay className="text-white text-2xl mt-1 opacity-80"  />
+                        </div>
+                        </div>
+
                     </div>
                 </div>
 
