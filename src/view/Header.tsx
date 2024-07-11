c6d5ee1d573cc6d7673690bed7fa5d1b16afd2fb
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-07-11 13:25:00 +0530

    Update Header.tsx

--- src/view/Header.tsx
+++ src/view/Header.tsx
@@ -19,7 +19,7 @@ function Header() {
   return (
     <header className="sticky flex -mb-[8px] px-5 py-4 md:px-[120px] md:py-8 h-[130px] md:h-[136px] justify-center items-center  z-30 bg-pink-0  ">
       {/* <img className="h-10 w-16 md:h-16 md:w-24" src={logo} alt="" /> */}
-      <a href="https://www.juleo.club/">
+      <a href="https://juleo-seven.vercel.app/">
         <img className="w-40 h-24 " src={logo} alt="" />
       </a>
       {width <= mediumScreen ? (
