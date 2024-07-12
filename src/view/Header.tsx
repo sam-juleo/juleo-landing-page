95dd9f09c5289719b5c7c5257faf43a94857c7de
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-07-12 12:01:04 +0530

    update
    Updated the image tag to go back to homepage

--- src/view/Header.tsx
+++ src/view/Header.tsx
@@ -20,7 +20,7 @@ function Header() {
     <header className="sticky flex -mb-[8px] px-5 py-4 md:px-[120px] md:py-8 h-[130px] md:h-[136px] justify-center items-center  z-30 bg-pink-0  ">
       {/* <img className="h-10 w-16 md:h-16 md:w-24" src={logo} alt="" /> */}
       <Link to="/">
-        <img className="w-40 h-24 " src={logo} alt="" />
+         <img className="w-40 h-24 " src={logo} alt="" />
       </Link>
       {width <= mediumScreen ? (
         <HamburgerIcon
